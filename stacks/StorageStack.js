18a3748485ca13732d38782e25713322ac3e4b39
yousef <yousefdergham@protonmail.com>
2022-01-31 18:17:02 +0200

    c

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -14,8 +14,19 @@ this.table = new sst.Table(this, "Notes", {
     },
     primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
 });
-// Create an S3 bucket
-this.bucket = new sst.Bucket(this, "Uploads");
+this.bucket = new sst.Bucket(this, "Uploads", {
+    s3Bucket: {
+      // Allow client side access to the bucket from a different domain
+      cors: [
+        {
+          maxAge: 3000,
+          allowedOrigins: ["*"],
+          allowedHeaders: ["*"],
+          allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+        },
+      ],
+    },
+  });
 
 }
 }
